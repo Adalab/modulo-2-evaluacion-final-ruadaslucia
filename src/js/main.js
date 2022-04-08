8f626a38d668be45f95cba5ea69e0dc3ee8fcb08
lucia <ruadaslucia@gmail.com>
2022-04-08 09:08:06 +0200

    botones

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,4 @@
 'use strict';
-console.log('holis');
+const inputSearch = document.querySelector('.js-inputSearch');
+const searchButton = document.querySelector('.js-searchButton');
+const resetButton = document.querySelector('.js-resetButton');
