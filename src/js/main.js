52958ef0f64f2aae2b1949583db3b40acad8bbd5
lucia <ruadaslucia@gmail.com>
2022-04-08 11:08:30 +0200

    added const

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 'use strict';
+const inputUser = document.querySelector('.js-inputSearchUser');
+const searchButton = document.querySelector('.js-searchButton ');
+const resetButton = document.querySelector('.js-resetButton ');
