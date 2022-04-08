54c659f2a045a87996f6103a22ba09b021aa1fa9
lucia <ruadaslucia@gmail.com>
2022-04-08 09:44:32 +0200

    minimal css to see better

--- README.md
+++ README.md
@@ -10,6 +10,12 @@
 - fotos eval terminada
   -conclusiones
 
+TAREAS A HACER
+
+- HTML MINIMO
+
+- al final de todo todisisimo meter css
+
 # Adalab web starter kit
 
 > **NOTA:**
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,1 @@
 'use strict';
-const inputSearch = document.querySelector('.js-inputSearch');
-const searchButton = document.querySelector('.js-searchButton');
-const resetButton = document.querySelector('.js-resetButton');
