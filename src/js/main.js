5e46939eeaff8c266923c8319f5831883817da4b
lucia <ruadaslucia@gmail.com>
2022-04-11 10:04:41 +0200

    clean code from comments and console logs

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 'use strict';
-//--recupero los favoritos del local storage
 let favorites = localStorage.getItem('favorites');
-//constantes
+
 const favoriteList = document.querySelector('.js-favorites');
 const inputSearch = document.querySelector('.js-input');
 const buttonSearch = document.querySelector('.js-button-search');
 const drinkList = document.querySelector('.js-drinkList');
 const buttonReset = document.querySelector('.js-button-reset');
 
-//if para
 if (favorites === null) {
   favorites = [];
 } else {
@@ -18,42 +16,33 @@ if (favorites === null) {
     favoriteList.appendChild(favLiElement);
   }
 }
-//evento escucha click
 buttonSearch.addEventListener('click', handleClickBtnSearch);
-//funcion manejadora del boton buscar
 function handleClickBtnSearch(event) {
   drinkList.innerHTML = '';
   event.preventDefault();
-  //variable con el valor del input
   let searchTerm = inputSearch.value;
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
-  //console.log(searchTerm);
-  //fetch para traer la info de la api
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       for (const drink of data.drinks) {
-        let liDrink = createBaseLiDrink(drink); //crea una lista en drink
-        let buttonFav = document.createElement('button'); //crea boton fav
+        let liDrink = createBaseLiDrink(drink);
+        let buttonFav = document.createElement('button');
         buttonFav.innerText = 'fav';
-        liDrink.appendChild(buttonFav); //metemos el boton fav en lista
+        liDrink.appendChild(buttonFav);
         buttonFav.addEventListener('click', () => {
-          //evento escucha boton fav
-          addDrinkToFavorites(drink, liDrink); //funcion que añade drink a favoritos
+          addDrinkToFavorites(drink, liDrink);
         });
         if (existsInFavorites(drink)) {
-          //si existe esta funcion
-          removeFavButttonAndChangeColors(liDrink); //se aplica esta otra funcion
+          removeFavButttonAndChangeColors(liDrink);
         } else {
-          //si no
-          liDrink.classList.add('regularItem'); //se agrega esta clase
+          liDrink.classList.add('regularItem');
         }
 
-        drinkList.appendChild(liDrink); //adjunta la bebida a drinkList
+        drinkList.appendChild(liDrink);
       }
     });
 }
-//funcion para ver si un favorito existe en la lista de favoritos por medio del id de la bebida
 function existsInFavorites(drink) {
   for (const favorite of favorites) {
     if (drink.idDrink === favorite.idDrink) {
@@ -63,17 +52,16 @@ function existsInFavorites(drink) {
   return false;
 }
 
-//funcion que contiene la creacion de una estructura de bebida
 function createBaseLiDrink(drink) {
-  let liDrink = document.createElement('li'); //se crea el li
+  let liDrink = document.createElement('li');
 
-  let liImageDrink = document.createElement('img'); //se crea la imagen
-  liImageDrink.setAttribute('src', drink.strDrinkThumb); //se le crea un src asociado a la imagen
-  liDrink.appendChild(liImageDrink); // se adjunta la imagen a la lista
+  let liImageDrink = document.createElement('img');
+  liImageDrink.setAttribute('src', drink.strDrinkThumb);
+  liDrink.appendChild(liImageDrink);
 
-  let liNameDrink = document.createElement('p'); //se crea un elemento parrafo
-  liNameDrink.innerText = drink.strDrink; //se le crea el texto del parrafo
-  liDrink.appendChild(liNameDrink); //se adjunta el parrafo a la lista
+  let liNameDrink = document.createElement('p');
+  liNameDrink.innerText = drink.strDrink;
+  liDrink.appendChild(liNameDrink);
 
   return liDrink;
 }
@@ -81,7 +69,6 @@ function createBaseLiDrink(drink) {
 function addDrinkToFavorites(drink, liDrinkResult) {
   removeFavButttonAndChangeColors(liDrinkResult);
 
-  //crear el fav y añadirlo a la lista de favs
   let liDrink = createLiFavoriteElement(drink);
   favoriteList.appendChild(liDrink);
   favorites.push(drink);
@@ -93,21 +80,16 @@ function createLiFavoriteElement(drink) {
   return liDrink;
 }
 
-//funcion para llenar el local storage
 function addToLocalStorage(favorites) {
   localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
-//funcion para cambiar colores y remover botón fav de la lista general
 function removeFavButttonAndChangeColors(liDrink) {
-  //ocultar boton favorito
   let liFavbutton = liDrink.querySelector('button');
   liFavbutton.classList.add('notDisplay');
-  //poner estilos al fav en el list de resultados
   liDrink.classList.add('clickedFavorites');
   liDrink.classList.remove('regularItem');
 }
-
 buttonReset.addEventListener('click', handleClickButtonReset);
 
 function handleClickButtonReset() {
